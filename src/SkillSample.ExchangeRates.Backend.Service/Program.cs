0dce4e7937b7034de6f00626121a1a111a7ee74a
Wojciech Biegała <wojciech.biegala@gmail.com>
2023-10-23 22:24:00 +0200

    CORS for ui fix

--- src/SkillSample.ExchangeRates.Backend.Service/Program.cs
+++ src/SkillSample.ExchangeRates.Backend.Service/Program.cs
@@ -26,8 +26,19 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors();
 
 if (app.Environment.IsDevelopment())
 {
